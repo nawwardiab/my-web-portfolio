6bfe76a7a4fea2eab4f586abc1b9cc296b721905
Nawwar <nawwar.d92@gmail.com>
2024-09-28 08:54:49 +0200

    animated loading skill

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+const burgerMenu = document.querySelector(".burger-menu"); // Fix the correct selector for the burger menu
 const navBar = document.querySelector(".navbar");
+const navbarList = document.querySelector(".navbar-list");
+const navLinks = document.querySelectorAll(".navbar-list--link"); // Select all links in the navbar
+const body = document.querySelector("body");
 const main = document.querySelector(".main");
 const skillsSection = document.querySelector(".skills-section");
 const experienceSection = document.querySelector(".experience-section");
@@ -6,8 +10,6 @@ const projectsSection = document.querySelector(".projects-section");
 const contactSection = document.querySelector(".contact-section");
 const btns = document.querySelectorAll(".btn"); // Selecting all buttons
 const plusIcon = document.querySelector(".exp-ticket.contact .fa-plus"); // Select the plus icon
-
-const navbarHeight = navBar.offsetHeight;
 
 document.addEventListener("DOMContentLoaded", function () {
   // Navigation bar click event
@@ -69,4 +71,22 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   }
+
+  // Toggle the navbar on burger menu click
+  burgerMenu.addEventListener("click", () => {
+    navbarList.classList.toggle("open");
+    burgerMenu.classList.toggle("open");
+    document.body.classList.toggle("no-scroll"); // Optional: prevent body from scrolling when menu is open
+  });
+
+  // Close the navbar when a link is clicked
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      console.log("Nav link clicked!"); // Debugging line
+      navbarList.classList.remove("open");
+      burgerMenu.classList.remove("open");
+      document.body.classList.remove("no-scroll"); // Re-enable scrolling
+      // Allow default action to take place (i.e., navigate to the section)
+    });
+  });
 });
